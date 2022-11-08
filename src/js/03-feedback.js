2d9dcba9a53096a2cdb70acb0ce01eb2acea73ba
Oleksandr Deoneha <oleksandr.deoneha@gmail.com>
2022-11-08 21:50:45 +0200

    add task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(inputMessage, 500));
+const formRecord = {};
+
+function inputMessage(event) {
+  formRecord[event.target.name] = event.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formRecord));
+}
+
+getData();
+
+function getData() {
+  const getTextData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+  if (getTextData) {
+    form.email.value = getTextData.email;
+    form.message.value = getTextData.message;
+  }
+}
+
+form.addEventListener('submit', onSubmit);
+function onSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, message },
+  } = event.currentTarget;
+  const data = {
+    email: email.value,
+    message: message.value,
+  };
+  console.log(data);
+  event.currentTarget.reset();
+  localStorage.clear();
+}
